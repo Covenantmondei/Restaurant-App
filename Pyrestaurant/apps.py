ce495f1072187c39ef180ee67bc270c6f8bd9b96
Covenant Monday <covenantmonday863@gmail.com>
2025-07-21 20:46:09 +0100

    added app

---
+++ Pyrestaurant/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PyrestaurantConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Pyrestaurant'
---
+++ Pyrestaurant/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    type = models.CharField(max_length=50)
+    description = models.TextField()
+    rating = models.FloatField()
+    image = models.ImageField(upload_to='restaurant_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    restaurant = models.ForeignKey(Restaurant, related_name='foods', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='food_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Customers(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    restaurant = models.ForeignKey(Restaurant, related_name='reviews', on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customers, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.customer.name} for {self.restaurant.name}'
