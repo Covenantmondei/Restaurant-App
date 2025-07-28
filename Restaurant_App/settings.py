fc6f7119e00d91f16caaa5d856b79d75a9a4b6f5
Idaraobong <idaraobong05@gmail.com>
2025-07-28 17:58:43 +0100

    Finished

--- Restaurant_App/settings.py
+++ Restaurant_App/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 
 
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'https://restaurant-app-6rtf.onrender.com',
     'http://localhost:8000',
