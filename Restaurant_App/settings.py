09c96a588bcdbcd60e3eb66b945574aee31722f6
Idaraobong <idaraobong05@gmail.com>
2025-07-28 17:11:20 +0100

    Finished

--- Restaurant_App/settings.py
+++ Restaurant_App/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--eaaqax=dd5k@xns)ywgcl232qn!c5cq@@!v)(wlnb9mi_s16('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
